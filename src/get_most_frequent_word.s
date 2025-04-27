b465af233c38b0dee94043434e7e67c4cbc28e82
zinanfu <fuzinan23@mails.ucas.ac.cn>
2025-04-27 07:33:05 -0400

    style: convert spaces to tabs

--- src/get_most_frequent_word.s
+++ src/get_most_frequent_word.s
@@ -1,119 +1,114 @@
-    .section .rodata
+	.section .rodata
 	.align 3
-    msg1: .asciz "r"
+	msg1: .asciz "r"
 	.align 3
-    msg2: .asciz "Can not open file\n"
+	msg2: .asciz "Can not open file\n"
 	.section .text
 	.globl get_most_frequent_word
-    .type get_most_frequent_word, @function
+	.type get_most_frequent_word, @function
 get_most_frequent_word:
-    li      t0, 4128            # t0 = 4128
-    sub     sp, sp, t0          # sp -= 4128 
+	li		t0, 4128			# t0 = 4128
+	sub		sp, sp, t0			# sp -= 4128 
 	sd		ra, 8(sp)
 	sd		s0, 0(sp)
-    addi    s0, sp, 16          # char line[4096]
-        
-    
-    
-    mv      a0, a0              # *filename
-    la      a1, msg1            # "r"    
-    call    fopen               # fopen
+	addi	s0, sp, 16 			# char line[4096]
+	
+	mv		a0, a0				# *filename
+	la		a1, msg1			# "r"		
+	call	fopen				# fopen
 
-    bnez    a0, if_end
+	bnez		a0, if_end
 
-    la      a0, msg2
-    call    perror
+	la		a0, msg2
+	call	perror
 
-    li      a0, 0               # return NULL
-    j       end
+	li		a0, 0				# return NULL
+	j		end
 
-    
-    .local if_end
+	.local if_end
 if_end:
-    mv      t0, a0             # store fp in t0
-    .local while
+	mv		t0, a0				# store fp in t0
+	.local while
 while:
-    
-    addi    sp, sp, -16         # store *line
-    sd      s0, 0(sp)
-    sd      t0, 8(sp)
-    
-    mv      a0, s0
-    li      a1, 4096
-    mv      a2, t0
-    call    fgets
+	
+	addi	sp, sp, -16			# store *line
+	sd		s0, 0(sp)
+	sd		t0, 8(sp)
+	
+	mv		a0, s0
+	li		a1, 4096
+	mv		a2, t0
+	call	fgets
 
-    ld      t0, 8(sp)
-    ld      s0, 0(sp)
-    addi    sp, sp, 16
-    
-    beqz    a0, while_end
+	ld		t0, 8(sp)
+	ld		s0, 0(sp)
+	addi	sp, sp, 16
+	
+	beqz	a0, while_end
 
-    addi    sp, sp, -16
-    sd      s0, 0(sp)
-    sd      t0, 8(sp)
+	addi	sp, sp, -16
+	sd		s0, 0(sp)
+	sd		t0, 8(sp)
 
-    mv      a0, s0              # store fp
-    call    processLine
+	mv		a0, s0				# store fp
+	call	processLine
 
-    ld      s0, 0(sp)
-    ld      t0, 8(sp)
-    addi    sp, sp, 16
+	ld		s0, 0(sp)
+	ld		t0, 8(sp)
+	addi	sp, sp, 16
 
-    j       while
+	j		while
 
-    .local while_end
+	.local while_end
 while_end:
-    mv      a0, t0
-    call    fclose
+	mv		a0, t0
+	call		fclose
 
-    li      t1, 0               # mostWord = NULL
-    li      t2, 0               # maxcount = 0
+	li		t1, 0				# mostWord = NULL
+	li		t2, 0				# maxcount = 0
 
-    li      t3, 0
+	li		t3, 0
 
-    .local for_loop
+	.local for_loop
 for_loop:
-    li      t4, 10007
-    bgeu    t3, t4, for_loop_end
+	li		t4, 10007
+	bgeu	t3, t4, for_loop_end
 
-    slli    a3, t3, 3           # address need *8
-    la      t5, hashTable
-    add     t5, t5, a3          # *node
-    ld      t6, 0(t5)           # node
-    
-    .local while_loop
+	slli	a3, t3, 3			# address need *8
+	la		t5, hashTable
+	add		t5, t5, a3			# *node
+	ld		t6, 0(t5)			# node
+	
+	.local while_loop
 while_loop:
-    
-    beqz    t6, while_loop_end
-    lw      t0, 8(t6)           # count
-    bleu    t0, t2, if_end1
-    mv      t2, t0
-    ld      a1, 0(t6)           # node -> *word
-    mv      t1, a1
+	
+	beqz		t6, while_loop_end
+	lw		t0, 8(t6)			# count
+	bleu	t0, t2, if_end1
+	mv		t2, t0
+	ld		a1, 0(t6)			# node -> *word
+	mv		t1, a1
 
-    .local if_end1
+	.local if_end1
 if_end1:
-    ld      t6, 16(t6)          # node -> next
-    j       while_loop
-    .local while_loop_end
+	ld		t6, 16(t6)			# node -> next
+	j		while_loop
+	.local while_loop_end
 while_loop_end:
 
-
-    addi    t3, t3, 1
-    j       for_loop
-    .local for_loop_end
+	addi		t3, t3, 1
+	j		for_loop
+	.local for_loop_end
 for_loop_end:
 
+	mv		a0, t1				# return mostWord
+	mv		a1, t2				# count_out
 
-    mv      a0, t1              # return mostWord
-    mv      a1, t2              # count_out
-
-    .local end
+	.local end
 end:
-    nop
+	nop
 	ld		ra, 8(sp)
 	ld		s0, 0(sp)
-	li      t0, 4128            # t0 = 4128
-    add     sp, sp, t0          # sp += 4128
+	li		t0, 4128			# t0 = 4128
+	add		sp, sp, t0			# sp += 4128
 	ret
--- src/processLine.s
+++ src/processLine.s
@@ -1,161 +1,161 @@
 	.section .text
 	.globl processLine
-    .type processLine, @function
+	.type processLine, @function
 processLine:
-    addi	sp, sp, -144
+	addi	sp, sp, -144
 	sd		ra, 8(sp)
 	sd		s0, 0(sp)  
 
-    li      t0, 0       # i
-    li      t1, 0       # j
+	li		t0, 0		    # i
+	li		t1, 0		    # j
 
-    addi    s0, sp, 16
+	addi	s0, sp, 16
 
-    mv      a5, a0      # *line
+	mv		a5, a0		    # *line
 
-    .local for_loop
-for_loop:               
-                 
-    add     t5, t0, a5
-    lb      t3, 0(t5)        # line[i]
-    beqz    t3, for_loop_end  
+	.local for_loop
+for_loop:		
+	
+	add		t5, t0, a5
+	lb		t3, 0(t5)		# line[i]
+	beqz	t3, for_loop_end  
 
-    addi	sp, sp, -80
+	addi	sp, sp, -80
 	sd		a1, 0(sp)  
-    sd      a5, 8(sp)
-    sd      t1, 16(sp)
-    sd      t3, 24(sp)
-    sd      t3, 32(sp)
-    sd      t4, 40(sp)
-    sd      t5, 48(sp)
-    sd      t6, 56(sp)
-    sd      t0, 64(sp)
+	sd		a5, 8(sp)
+	sd		t1, 16(sp)
+	sd		t3, 24(sp)
+	sd		t3, 32(sp)
+	sd		t4, 40(sp)
+	sd		t5, 48(sp)
+	sd		t6, 56(sp)
+	sd		t0, 64(sp)
 
-    mv      a0, t3           # parameter line[i]
-    call    isalpha       
+	mv		a0, t3		    # parameter line[i]
+	call	isalpha		
 
-    ld      t0, 64(sp)
-    ld      t6, 56(sp)
-    ld      t5, 48(sp)
-    ld      t4, 40(sp)
-    ld      t3, 32(sp)
-    ld      t2, 24(sp)
-    ld      t1, 16(sp)
-    ld      a5, 8(sp)
-    ld      a1, 0(sp)
-    addi    sp, sp, 80 
+	ld		t0, 64(sp)
+	ld		t6, 56(sp)
+	ld		t5, 48(sp)
+	ld		t4, 40(sp)
+	ld		t3, 32(sp)
+	ld		t2, 24(sp)
+	ld		t1, 16(sp)
+	ld		a5, 8(sp)
+	ld		a1, 0(sp)
+	addi	sp, sp, 80 
 
-    beq     a0, x0, else_if
+	beq		a0, x0, else_if
 
-    addi	sp, sp, -80
+	addi	sp, sp, -80
 	sd		a1, 0(sp)  
-    sd      a5, 8(sp)
-    sd      t1, 16(sp)
-    sd      t3, 24(sp)
-    sd      t3, 32(sp)
-    sd      t4, 40(sp)
-    sd      t5, 48(sp)
-    sd      t6, 56(sp)
-    sd      t0, 64(sp)
+	sd		a5, 8(sp)
+	sd		t1, 16(sp)
+	sd		t3, 24(sp)
+	sd		t3, 32(sp)
+	sd		t4, 40(sp)
+	sd		t5, 48(sp)
+	sd		t6, 56(sp)
+	sd		t0, 64(sp)
 
-    mv      a0, t3           # parameter line[i]
-    call    tolower          
-    
-    ld      t0, 64(sp)
-    ld      t6, 56(sp)
-    ld      t5, 48(sp)
-    ld      t4, 40(sp)
-    ld      t3, 32(sp)
-    ld      t2, 24(sp)
-    ld      t1, 16(sp)
-    ld      a5, 8(sp)
-    ld      a1, 0(sp)
-    addi    sp, sp, 80 
+	mv		a0, t3		    # parameter line[i]
+	call    tolower		
+	
+	ld		t0, 64(sp)
+	ld		t6, 56(sp)
+	ld		t5, 48(sp)
+	ld		t4, 40(sp)
+	ld		t3, 32(sp)
+	ld		t2, 24(sp)
+	ld		t1, 16(sp)
+	ld		a5, 8(sp)
+	ld		a1, 0(sp)
+	addi	sp, sp, 80 
 
-    add     t5, s0, t1
-    sb      a0, 0(t5)        # word[j] = tolower(line[i]);
-    addi    t1, t1, 1        # j++
-    j       else_if_end   
+	add		t5, s0, t1
+	sb		a0, 0(t5)		# word[j] = tolower(line[i]);
+	addi	t1, t1, 1		# j++
+	j		else_if_end		
 
-    .local else_if   
-else_if:     
-    blez    t1, if_end 
-    
-    add     t5, s0, t1          
-    sb      x0, 0(t5)        # word[j] = '\0'
+	.local else_if		
+else_if:		
+	blez	t1, if_end 
+	
+	add		t5, s0, t1		
+	sb		x0, 0(t5)		# word[j] = '\0'
 
-    addi	sp, sp, -80
+	addi	sp, sp, -80
 	sd		a1, 0(sp)  
-    sd      a5, 8(sp)
-    sd      t1, 16(sp)
-    sd      t3, 24(sp)
-    sd      t3, 32(sp)
-    sd      t4, 40(sp)
-    sd      t5, 48(sp)
-    sd      t6, 56(sp)
-    sd      t0, 64(sp)
+	sd		a5, 8(sp)
+	sd		t1, 16(sp)
+	sd		t3, 24(sp)
+	sd		t3, 32(sp)
+	sd		t4, 40(sp)
+	sd		t5, 48(sp)
+	sd		t6, 56(sp)
+	sd		t0, 64(sp)
 
-    mv      a0, s0           # parameter *word
-    call    insertWord       
+	mv		a0, s0		    # parameter *word
+	call	insertWord		
 
-    ld      t0, 64(sp)
-    ld      t6, 56(sp)
-    ld      t5, 48(sp)
-    ld      t4, 40(sp)
-    ld      t3, 32(sp)
-    ld      t2, 24(sp)
-    ld      t1, 16(sp)
-    ld      a5, 8(sp)
-    ld      a1, 0(sp)
-    addi    sp, sp, 80  
+	ld		t0, 64(sp)
+	ld		t6, 56(sp)
+	ld		t5, 48(sp)
+	ld		t4, 40(sp)
+	ld		t3, 32(sp)
+	ld		t2, 24(sp)
+	ld		t1, 16(sp)
+	ld		a5, 8(sp)
+	ld		a1, 0(sp)
+	addi	sp, sp, 80  
 
-    li      t1, 0            # j = 0
+	li		t1, 0		    # j = 0
 
-    .local if_end
+	.local if_end
 if_end:
 
-    .local else_if_end
+	.local else_if_end
 else_if_end:
 
-    addi    t0, t0, 1        # i++ 
+	addi	t0, t0, 1		# i++ 
 
-    j       for_loop
-    .local for_loop_end
+	j		for_loop
+	.local for_loop_end
 for_loop_end:
 
-    blez    t1, if_end2
-    
-    add     t5, s0, t1          
-    sb      x0, 0(t5)        # word[j] = '\0'444  
+	blez	t1, if_end2
+	
+	add		t5, s0, t1		
+	sb		x0, 0(t5)		# word[j] = '\0'444  
 
-    addi	sp, sp, -80
+	addi	sp, sp, -80
 	sd		a1, 0(sp)  
-    sd      a5, 8(sp)
-    sd      t1, 16(sp)
-    sd      t3, 24(sp)
-    sd      t3, 32(sp)
-    sd      t4, 40(sp)
-    sd      t5, 48(sp)
-    sd      t6, 56(sp)
-    sd      t0, 64(sp)
+	sd		a5, 8(sp)
+	sd		t1, 16(sp)
+	sd		t3, 24(sp)
+	sd		t3, 32(sp)
+	sd		t4, 40(sp)
+	sd		t5, 48(sp)
+	sd		t6, 56(sp)
+	sd		t0, 64(sp)
 
-    mv      a0, s0           # parameter *word
-    call    insertWord    
+	mv		a0, s0		    # parameter *word
+	call	insertWord		
 
-    ld      t0, 64(sp)
-    ld      t6, 56(sp)
-    ld      t5, 48(sp)
-    ld      t4, 40(sp)
-    ld      t3, 32(sp)
-    ld      t2, 24(sp)
-    ld      t1, 16(sp)
-    ld      a5, 8(sp)
-    ld      a1, 0(sp)
-    addi    sp, sp, 80  
+	ld		t0, 64(sp)
+	ld		t6, 56(sp)
+	ld		t5, 48(sp)
+	ld		t4, 40(sp)
+	ld		t3, 32(sp)
+	ld		t2, 24(sp)
+	ld		t1, 16(sp)
+	ld		a5, 8(sp)
+	ld		a1, 0(sp)
+	addi	sp, sp, 80  
 
-    .local if_end2
+	.local if_end2
 if_end2:
-    nop
+	nop
 	ld		ra, 8(sp)
 	ld		s0, 0(sp)
 	addi	sp, sp, 144
